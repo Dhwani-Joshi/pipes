37ab8427e48b9c06db4bb8c532696265eb7855cf
jalpesh <jalpesh.fullstack@gmail.com>
2021-02-08 17:04:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:  './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  names = [];
+  constructor() {
+    this.names = this.getNames();
+}
+getNames() {
+    return [
+        { 'name': 'dhananjay Kumar', },
+        { 'name': 'jason beres' },
+        { 'name': 'adam jafe' }
+    ];
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FirstCharacter } from './firstcharacter.pipe'
 
 import { AppComponent } from './app.component';
+import { SquarePipe } from './square.pipe';
+import { PowerPipe } from './power.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCharacter,
+    SquarePipe,
+    PowerPipe
   ],
   imports: [
     BrowserModule
